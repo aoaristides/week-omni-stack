9df9a0105986d854a350d381a6cd38fac80a8dca
Anderson O. Aristides <anderson.aristides@fh.com.br>
2019-06-14 00:24:55 -0300

    feat:Construindo a interface web com React

---
+++ frontend/src/pages/Feed.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+
+import './Feed.css';
+
+import more from '../assets/more.svg';
+import like from '../assets/like.svg';
+import comment from '../assets/comment.svg';
+import send from '../assets/send.svg';
+
+class Feed extends Component {
+    render() {
+        return (
+            <section id="post-list">
+                <article>
+                    <header>
+                        <div className="user-info">
+                            <span>Anderson Aristides</span>
+                            <span className="place">Curitiba</span>
+                        </div>
+                        <img src={more} alt="Mais" />
+                    </header>
+
+                    <img src="http://localhost:3333/files/giulia_costa_-003.jpg" alt="Giulia Costa" />
+               
+                    <footer>
+                        <div className="actions">
+                            <img src={like} alt="" />
+                            <img src={comment} alt="" />
+                            <img src={send} alt="" />
+                        </div>
+
+                        <strong>900 curtidas</strong>
+
+                        <p>
+                            Um post muito massa da Giulia Costa!
+                            <span>#giulia #bunda #top</span>
+                        </p>
+                    </footer>               
+                </article>
+                <article>
+                    <header>
+                        <div className="user-info">
+                            <span>Anderson Aristides</span>
+                            <span className="place">Curitiba</span>
+                        </div>
+                        <img src={more} alt="Mais" />
+                    </header>
+
+                    <img src="http://localhost:3333/files/giulia_costa_-003.jpg" alt="Giulia Costa" />
+               
+                    <footer>
+                        <div className="actions">
+                            <img src={like} alt="" />
+                            <img src={comment} alt="" />
+                            <img src={send} alt="" />
+                        </div>
+
+                        <strong>900 curtidas</strong>
+
+                        <p>
+                            Um post muito massa da Giulia Costa!
+                            <span>#giulia #bunda #top</span>
+                        </p>
+                    </footer>               
+                </article>
+            </section>
+        );
+    }
+}
+
+export default Feed;
